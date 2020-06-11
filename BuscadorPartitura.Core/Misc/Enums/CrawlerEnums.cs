347374ed4a1ef04e2a85729b3a47d2d09683f533
Rogerson Reis Nazario <M6024365@mgmt.tlrg.com>
2020-06-10 23:04:58 -0300

    Adding Enum

---
+++ BuscadorPartitura.Core/Misc/Enums/CrawlerEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuscadorPartitura.Core.Misc.Enums
+{
+    public class CrawlerEnums
+    {
+        public enum TipoCrawler
+        {
+            ChoraMeuCavaco = 0
+        }
+    }
+}
