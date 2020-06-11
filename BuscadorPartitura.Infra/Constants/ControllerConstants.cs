b529f45662c36bd59b97347fd066aef76ba67803
Rogerson Reis Nazario <M6024365@mgmt.tlrg.com>
2020-06-10 23:03:34 -0300

    adjusting constant places

---
+++ BuscadorPartitura.Infra/Constants/ControllerConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuscadorPartitura.Infra.Constants
+{
+    public class ControllerConstants
+    {
+        public const string CrawlerExeName = "BuscadorPartitura.Crawler.exe";
+    }
+}
--- BuscadorPartitura.Infra/Constants/FunctionsConstants.cs
+++ BuscadorPartitura.Infra/Constants/FunctionsConstants.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BuscadorPartitura.Infra.Misc
+namespace BuscadorPartitura.Infra.Constants
 {
     public class FunctionsConstants
     {
