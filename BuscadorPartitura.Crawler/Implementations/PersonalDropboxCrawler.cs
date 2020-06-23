8dac5b939890aff5670a313275d954e843369b91
Rogerson Reis Nazario <M6024365@mgmt.tlrg.com>
2020-06-23 11:21:46 -0300

    Dropbox crawler implementing

---
+++ BuscadorPartitura.Crawler/Implementations/PersonalDropboxCrawler.cs
@@ -0,0 +1,21 @@
+﻿using BuscadorPartitura.Crawler.Interfaces;
+using BuscadorPartitura.Crawler.Model;
+using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscadorPartitura.Crawler.Implementations
+{
+    public class PersonalDropboxCrawler : BaseCrawler, ICrawler
+    {
+        public PersonalDropboxCrawler(Search search) : base(search) { }
+        
+        public override async Task<List<string>> GetImagesAsync()
+        {
+#warning ROGIM: Pegar do dropbox as imagens
+            throw new NotImplementedException();
+        }
+    }
+}
