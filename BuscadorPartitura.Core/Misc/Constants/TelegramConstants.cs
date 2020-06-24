abbafb4babb9f72f8f23e2ebe5635967de037271
Rogerson Reis Nazario <M6024365@mgmt.tlrg.com>
2020-06-24 13:03:10 -0300

    Telegram constants

---
+++ BuscadorPartitura.Core/Misc/Constants/TelegramConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuscadorPartitura.Core.Misc.Constants
+{
+    public class TelegramConstants
+    {
+        public const string OrquestradorDefaultUrl = "http://localhost:8080/api/";
+        public static readonly string OrquestradorGetSheetUrl = $"{OrquestradorDefaultUrl}/GetSheet";
+    }
+}
